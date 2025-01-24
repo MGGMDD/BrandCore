58069fae4ce27acdba91d7d7307157899334e9be
MGGMDD <mariagrinian@gmail.com>
2025-01-24 02:45:50 +0100

    [MGM] Add output ports and adapters for the Prices model.

--- src/main/java/com/inditex/brandcore/infrastructure/adapter/PricesRepositoryAdapter.java
+++ src/main/java/com/inditex/brandcore/infrastructure/adapter/PricesRepositoryAdapter.java
@@ -1,4 +1,28 @@
 package com.inditex.brandcore.infrastructure.adapter;
 
-public class PricesRepositoryAdapter {
+import com.inditex.brandcore.domain.model.Price;
+import com.inditex.brandcore.domain.ports.PricesRepository;
+import com.inditex.brandcore.infrastructure.mappers.PriceMapper;
+import com.inditex.brandcore.infrastructure.persistence.PriceEntity;
+import com.inditex.brandcore.infrastructure.repository.PriceJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class PricesRepositoryAdapter implements PricesRepository {
+    private final PriceJpaRepository priceJpaRepository;
+    private final PriceMapper priceMapper;
+
+    public PricesRepositoryAdapter(PriceJpaRepository priceJpaRepository, PriceMapper priceMapper) {
+        this.priceJpaRepository = priceJpaRepository;
+        this.priceMapper = priceMapper;
+    }
+
+    @Override
+    public Price findPriceByDate(LocalDateTime applicationDate, Integer productId, Integer brandId) {
+        PriceEntity entity =  priceJpaRepository.findHighestPriorityPrice(applicationDate,productId,brandId);
+        return priceMapper.toPrice(entity);
+    }
 }
--- src/main/java/com/inditex/brandcore/infrastructure/repository/PriceJpaRepository.java
+++ src/main/java/com/inditex/brandcore/infrastructure/repository/PriceJpaRepository.java
@@ -1,4 +1,37 @@
 package com.inditex.brandcore.infrastructure.repository;
 
-public interface PriceJpaRepository {
+import com.inditex.brandcore.infrastructure.persistence.PriceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+/**
+ * The interface Price jpa repository.
+ */
+@Repository
+public interface PriceJpaRepository extends JpaRepository<PriceEntity,Integer> {
+
+
+    /**
+     * Find highest priority price price entity.
+     *
+     * @param applicationDate the application date
+     * @param productId       the product id
+     * @param brandId         the brand id
+     * @return the price entity
+     */
+    @Query("SELECT p FROM PriceEntity p " +
+            "WHERE :applicationDate >= p.startDate AND :applicationDate <= p.endDate " +
+            "AND p.productId = :productId " +
+            "AND p.brand.id = :brandId " +
+            "AND p.priority = (SELECT MAX(p2.priority) FROM PriceEntity p2 " +
+            "WHERE :applicationDate >= p2.startDate AND :applicationDate <= p2.endDate " +
+            "AND p2.productId = :productId " +
+            "AND p2.brand.id = :brandId)")
+    PriceEntity findHighestPriorityPrice( @Param("applicationDate") LocalDateTime applicationDate,
+                                          @Param("productId") Integer productId,
+                                          @Param("brandId") Integer brandId);
 }
