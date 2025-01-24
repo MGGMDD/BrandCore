704f27ce72dd02dfba937af037dff02fabefba9f
MGGMDD <mariagrinian@gmail.com>
2025-01-24 02:47:49 +0100

    [MGM] Add use cases and their implementation.

--- src/main/java/com/inditex/brandcore/domain/service/CalculatePriceService.java
+++ src/main/java/com/inditex/brandcore/domain/service/CalculatePriceService.java
@@ -1,4 +1,46 @@
 package com.inditex.brandcore.domain.service;
 
-public class CalculatePriceService {
+import com.inditex.brandcore.application.usecases.CalculatePriceUseCase;
+import com.inditex.brandcore.domain.model.Brand;
+import com.inditex.brandcore.domain.model.Price;
+import com.inditex.brandcore.domain.model.response.ResponsePricesInfo;
+import com.inditex.brandcore.domain.ports.BrandRepository;
+import com.inditex.brandcore.domain.ports.PricesRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+/**
+ * The type Calculate price service.
+ */
+@Service
+@Slf4j
+public class CalculatePriceService implements CalculatePriceUseCase {
+    private final PricesRepository pricesRepository;
+    private final BrandRepository brandRepository;
+
+    public CalculatePriceService(PricesRepository pricesRepository, BrandRepository brandRepository) {
+        this.pricesRepository = pricesRepository;
+        this.brandRepository = brandRepository;
+    }
+
+    @Override
+    public ResponsePricesInfo callToPricesInfo(LocalDateTime applicationDate, Integer productId, Integer brandId) {
+        Brand brand = brandRepository.findById(brandId);
+        log.info("Se procede a obtener la informacion sobre la cadena {}",brand.getBrandName());
+        Price price = pricesRepository.findPriceByDate(applicationDate, productId, brandId);
+        return buildResponse(price, applicationDate);
+    }
+
+    private ResponsePricesInfo buildResponse(Price price, LocalDateTime applicationDate) {
+        return ResponsePricesInfo.builder()
+                .priceList(price.getPriceList())
+                .finalPrice(price.getPrice())
+                .brandId(price.getBrand().getId())
+                .productId(price.getProductId())
+                .applicationDate(applicationDate).build();
+    }
 }
